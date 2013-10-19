411c98c30a748994628a7159ba910153b604d3df
Marian Schubert <marian.schubert@gmail.com>
2013-10-19 08:35:06 +0200

    more async stuff

--- src/cljs/cljsfirst/first.cljs
+++ src/cljs/cljsfirst/first.cljs
@@ -1,11 +1,39 @@
 (ns cljsfirst.first
-  (:require [cljs.core.async :refer [chan close! timeout]])
+  (:require [cljs.core.async :refer [chan close! timeout put!]]
+            [goog.dom :as dom])
   (:require-macros [cljs.core.async.macros :as m :refer [go]]))
 
+;; -- https://github.com/swannodette/async-tests/blob/master/src/async_test/utils/helpers.cljs
+
+(defn by-id [id] (dom/getElement id))
+
+(defn set-html! [el s]
+  (aset el "innerHTML" s))
+
+(defn event-chan
+  ([type] (event-chan js/window type))
+  ([el type] (event-chan (chan) el type))
+  ([c el type]
+    (let [writer #(put! c %)]
+      (.addEventListener el type writer)
+      {:chan c
+       :unsubscribe #(.removeEventListener el type writer)})))
+
+;; --
+
 (defn log [msg]
-  (.write js/document msg))
+  (.log js/console msg))
+
+(let [start (event-chan (by-id "start") "click")]
+  (go
+    (while true
+      (log (<! (:chan start))))))
 
 (let [src (chan)]
+  (go
+    (while true
+      (>! src "PING")
+      (<! (timeout 800))))
   (go
     (while true
       (>! src "ping")
@@ -13,5 +41,4 @@
   (go
     (while true
       (log (<! src)))))
-
 
